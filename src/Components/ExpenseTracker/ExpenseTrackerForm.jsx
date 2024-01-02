9621d2999d3302272e8c20817a27e18a01d134a0
estney <esther.pxy@gmail.com>
2024-01-02 17:34:42 +0800

    set input validation for category, to prevent duplicate categories due to letter casing. format text for catergories in dropdown menu.

--- src/Components/ExpenseTracker/ExpenseTrackerForm.jsx
+++ src/Components/ExpenseTracker/ExpenseTrackerForm.jsx
@@ -39,7 +39,17 @@ export default function ExpenseTrackerForm() {
   };
 
   const handleAddCategory = () => {
-    if (newCategory.trim() !== "" && !categories.includes(newCategory)) {
+    //input validation
+    //change the current category to lowercase for comparison
+    const lowerCaseCategories = categories.map((category) =>
+      category.toLowerCase()
+    );
+    const categoryExists = lowerCaseCategories.includes(
+      newCategory.trim().toLowerCase()
+    );
+
+    //do now allow users to add category if new category is empty or already exists
+    if (newCategory.trim() !== "" && !categoryExists) {
       const categoriesRef = ref(database, "expenses-categories");
       const newCategoryData = {};
       newCategoryData[newCategory] = true;
@@ -54,7 +64,7 @@ export default function ExpenseTrackerForm() {
         .catch((error) => {
           console.error("Error adding new category to Firebase:", error);
         });
-    }
+    } else console.log("category is empty or already exists.");
   };
 
   useEffect(() => {
@@ -156,7 +166,8 @@ export default function ExpenseTrackerForm() {
             </option>
             {categories.map((cat, index) => (
               <option key={index} value={cat}>
-                {cat}
+                {/* standardise appearance of text */}
+                {cat.charAt(0).toUpperCase() + cat.slice(1).toLowerCase()}
               </option>
             ))}
           </select>
