4075897d2a6d1f9dc6d692a3f981f750f2f1357a
estney <esther.pxy@gmail.com>
2024-01-04 16:26:04 +0800

    add list of currency to the conversion

--- src/Components/ExpenseTracker/CurrencyConversion.jsx
+++ src/Components/ExpenseTracker/CurrencyConversion.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = "fca_live_NvQFhAJ7HtdJEVH8nfNWbHsjiRnRH3t3IsVILyjM";
 
 export default function CurrencyConversion({
   showCurrencyModal,
   handleCloseCurrencyModal,
 }) {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setFromCurrency] = useState(""); // handle input currency
+  const [convertedAmount, setConvertedAmount] = useState("");
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+
+  useEffect(() => {
+    // Fetch currencies from the API
+    const API_URL = `https://api.freecurrencyapi.com/v1/latest?apikey=${API_KEY}`;
+
+    axios
+      .get(API_URL)
+      .then((response) => {
+        const currencyData = response.data.data;
+        console.log("current list - data", currencyData);
+        const currencies = Object.keys(currencyData);
+        setCurrencyOptions(currencies);
+        console.log("currencies", currencies);
+      })
+      .catch((error) => {
+        console.error("Error fetching currencies:", error);
+        // Handle error state or display an error message for currency fetch failure
+      });
+  }, []);
+
   return (
     <>
       {showCurrencyModal && (
@@ -16,21 +43,26 @@ export default function CurrencyConversion({
             <p>Amount to be converted:</p>
             <input
               type="number"
-              // value={}
-              onChange={(e) => e.target.value}
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
               placeholder="Enter amount"
             />
             <p>From</p>
-            <input
-              type="text"
-              // value={}
-              onChange={(e) => e.target.value}
-              placeholder="Choose Currency"
-            />
-            <p>To SGD: </p>
-            <button>Calculate</button>
+            <select
+              value={fromCurrency}
+              onChange={(e) => setFromCurrency(e.target.value)}
+            >
+              <option value="">Choose Currency</option>
+              {currencyOptions.map((currency) => (
+                <option key={currency} value={currency}>
+                  {currency}
+                </option>
+              ))}
+            </select>
+            <p>To SGD: {convertedAmount}</p>
+            <button onClick="">Calculate</button>
             <br />
-            <button>Add to Expenses</button>
+            <button onClick="">Add to Expenses</button>
           </div>
         </div>
       )}
