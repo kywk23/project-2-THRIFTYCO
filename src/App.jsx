327045cb73cf4955c73f1218d85f1a9fd8e241c2
kywk23 <kelvinywk.work@outlook.com>
2024-01-05 11:46:16 +0800

    track errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
-
-import ExpenseTracker from "./Components/ExpenseTracker/ExpenseTracker.jsx";
+import BillSplitGroups from "./Components/BillSplit/BillSplit-Groups.jsx";
 
 function App() {
   return (
     <>
       <h1>Money Tracker </h1>
-      {/* <LandingPage />
-      <ErrorPage /> */}
+
       {/* <LogInSignUp /> */}
-      <ExpenseTracker />
+      {/* <ExpenseTracker /> */}
+      <BillSplitGroups />
     </>
   );
 }
