33d5cf5afac3a1c2e316c498eb23e89e88142d05
kywk23 <kelvinywk.work@outlook.com>
2024-01-10 01:12:03 +0800

    auth state done

--- src/Components/Context/AuthContext.jsx
+++ src/Components/Context/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { auth } from "../firebase";
 
 export const AuthContext = createContext();
 
@@ -8,6 +9,8 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case "LOGOUT":
       return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return { ...state, user: action.payload, authIsReady: true };
     default:
       return state;
   }
@@ -16,7 +19,16 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
+    authIsReady: false,
   });
+
+  useEffect(() => {
+    const unsub = auth.onAuthStateChanged((user) => {
+      dispatch({ type: "AUTH_IS_READY", payload: user });
+      unsub();
+    });
+  }, []);
+
   console.log("AuthContext state", state);
 
   return <AuthContext.Provider value={{ ...state, dispatch }}>{children}</AuthContext.Provider>;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,57 +4,59 @@ import { useAuthContext } from "./Hooks/useAuthContext";
 
 function Navbar() {
   const { logout } = useLogOut();
-  const { user } = useAuthContext();
+  const { user, authIsReady } = useAuthContext();
 
   return (
     <div className="navbar-bg">
       <div className="navbar">
         <h1 className="logo"> THRIFTY & CO. </h1>
-        <ul>
-          <li>
-            <NavLink to="/" activeclassname="active" className="nav-link">
-              Home
-            </NavLink>
-          </li>
-          <li className="active">
-            <NavLink to="/expensetracker" activeclassname="active" className="nav-link">
-              Expense Tracker
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/splitbill" activeclassname="active" className="nav-link">
-              Bill Splitter
-            </NavLink>
-          </li>
+        {authIsReady && (
+          <ul>
+            <li>
+              <NavLink to="/" activeclassname="active" className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="active">
+              <NavLink to="/expensetracker" activeclassname="active" className="nav-link">
+                Expense Tracker
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/splitbill" activeclassname="active" className="nav-link">
+                Bill Splitter
+              </NavLink>
+            </li>
 
-          {!user && (
-            <>
-              <li>
-                <NavLink to="/SignUp" activeclassname="active" className="nav-link">
-                  Sign Up
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/LogIn" activeclassname="active" className="nav-link">
-                  Log In
-                </NavLink>
-              </li>
-            </>
-          )}
+            {!user && (
+              <>
+                <li>
+                  <NavLink to="/SignUp" activeclassname="active" className="nav-link">
+                    Sign Up
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/LogIn" activeclassname="active" className="nav-link">
+                    Log In
+                  </NavLink>
+                </li>
+              </>
+            )}
 
-          {user && (
-            <>
-              <li style={{ color: "rgb(150, 100, 300)", textDecoration: "underline white" }}>
-                Hello, {user.displayName}{" "}
-              </li>
-              <button style={{ backgroundColor: "white" }} onClick={logout}>
-                Log Out
-              </button>
-            </>
-          )}
+            {user && (
+              <>
+                <li style={{ color: "rgb(150, 100, 300)", textDecoration: "underline white" }}>
+                  Hello, {user.displayName}{" "}
+                </li>
+                <button style={{ backgroundColor: "white" }} onClick={logout}>
+                  Log Out
+                </button>
+              </>
+            )}
 
-          {/* Add other navigation links as needed */}
-        </ul>
+            {/* Add other navigation links as needed */}
+          </ul>
+        )}
       </div>
     </div>
   );
